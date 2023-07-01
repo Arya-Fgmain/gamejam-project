129c85762a52bd504ede0064de7e468325239306
Arya <aryafg2004@gmail.com>
2023-07-01 11:55:33 -0700

    learned to insert regular surfaces
    next up is creating text

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ screen = pygame.display.set_mode((800, 400))    # display surface (width, height
 pygame.display.set_caption('paigaim')
 clock = pygame.time.Clock()                     # clock object
 
-test_surface = pygame.Surface((100, 200))       # creates a REGULAR surface
+#test_surface = pygame.Surface((100, 200))       # creates a REGULAR surface
                                                 # that we can put on the display surface
-test_surface.fill('Red')    
+
+sky_surface = pygame.image.load('graphics/Sky.png')    # literally loading an image using a relative path
+ground_surface = pygame.image.load('graphics/ground.png')
+#test_surface.fill('Red')    
+
 
 
 while True:
@@ -17,10 +21,13 @@ while True:
             pygame.quit()               # opposite of pygame.init() --> uninitializes everything
             exit()                      # (from sys) closes all processes that are running
 
-    screen.blit(test_surface, (200,100))    # blit = block image transfer
+    
+    screen.blit(sky_surface, (0,0))         # blit = block image transfer 
+                                            # blocking display surface transfer to put this on top
                                             # (surface, position - (x,y))
                                             # coordinate system starts from top-left! 
-
+                                            # note: the order of blitting stacks surfaces on top of one another
+    screen.blit(ground_surface, (0,300))
     # draw all elements & update everything
     pygame.display.update()             # updates display surface
                                         # and puts changes on display surface
