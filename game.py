8286ac9225b064dff8794b03b15bd028d11b0926
Arya <aryafg2004@gmail.com>
2023-07-01 22:55:43 -0700

    added motion for the enemy and jumping mechanism for the character

--- game.py
+++ game.py
@@ -1,18 +1,26 @@
 import pygame
 from sys import exit
 
+'''PRELIMINARIES'''
+
 pygame.init()
 
+# default surface for the game
 screen = pygame.display.set_mode((900, 400))
 pygame.display.set_caption('My Game')
 
+# clock to manage the game's frame rate in the event loop
 clock = pygame.time.Clock()
 
-font = pygame.font.Font(None, 100)
+# for deciding between game/game-over page
+game_active = True
 
-text_surface = font.render('My Game', True, 'Red')
+# game title
+font = pygame.font.Font(None, 100)
+text_surface = font.render('Hel', True, 'Red')
 text_rect = text_surface.get_rect(center = (450, 50))
 
+'''REGULAR SURFACES'''
 sky_surface = pygame.image.load('gpics/background.png')
 sky_rect = sky_surface.get_rect(topleft = (0, 0))
 
@@ -21,7 +29,9 @@ level_surface.fill('Grey')
 level_rect = level_surface.get_rect(midbottom = (450, 400))
 
 player_surface = pygame.image.load('gpics/player.png').convert_alpha()
-player_rect = player_surface.get_rect(midbottom = (50, 300))
+player_rect = player_surface.get_rect(midbottom = (150, 300))
+# player's gravity --> increases slowly
+player_gravity = 1
 
 enemy_surface = pygame.image.load('gpics/enemy_resized.png').convert_alpha()
 enemy_rect = enemy_surface.get_rect(midbottom = (450, 320))
@@ -31,23 +41,43 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        
+        if game_active:
+            # checking if either the character was clicked on or the space key was pressed via the event loop to jump
+            if player_rect.bottom >= 300 and ( (event.type == pygame.MOUSEBUTTONDOWN and player_rect.collidepoint(event.pos)) or (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE) ):
+                player_gravity = -20
+        else:
+            temp_surface = pygame.Surface((900, 400))
+            temp_surface.fill('White')
+            temp_rect = temp_surface.get_rect(topleft = (0,0))
+            screen.blit(temp_surface, temp_rect)
+            temp_content_surf = pygame.image.load('gpics/death.png').convert_alpha()
+            temp_content_rect = temp_content_surf.get_rect(center = (450, 200))
+            screen.blit(temp_content_surf, temp_content_rect)
     
+    # drawing the setting & text on the screen
     screen.blit(sky_surface, sky_rect)
-    pygame.draw.rect(screen, 'Orange', text_rect, 10)
-    pygame.draw.rect(screen, 'Black', text_rect)
+    pygame.draw.rect(screen, '#ffbf00', text_rect, 10)
+    pygame.draw.rect(screen, '#ffbf00', text_rect)
     screen.blit(text_surface, text_rect)
     screen.blit(level_surface, level_rect)
 
-    # if player_rect.colliderect(enemy_rect):
-    #     exit()
-
-    if player_rect.left > 900:
-        player_rect.left = 0
-    player_rect.right += 1
+    # applying gravity and making sure player doesn't go under the level
+    player_gravity += 1
+    player_rect.bottom += player_gravity
+    if player_rect.bottom >= 300:
+        player_rect.bottom = 300
     screen.blit(player_surface, player_rect)
-
+    
+    enemy_rect.x -= 7
+    if enemy_rect.x <= -130:
+        enemy_rect.x = 900
     screen.blit(enemy_surface, enemy_rect)
 
-    pygame.display.update()
+    # collision has happened
+    if enemy_rect.colliderect(player_rect):
+        game_active = False
 
-    clock.tick(350)
+    # updating the canvas with the newly applied changes at a rate of 1/60th of a second
+    pygame.display.update()
+    clock.tick(60)
